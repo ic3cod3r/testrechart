55fc81a875403bcd36e09a85c8e05ee3cb0d3d8f
Hunter Baker <hunter.05baker@gmail.com>
2022-06-09 16:46:14 -0500

    added timer/progress bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react'
 //import { PieChart } from 'recharts';
 import BarCharty from './barChart';
 import StockChart from './stockChart';
 import PieCharty from './pieChart';
+// import Timery from './timer';
+// import NoApproval from './timer';
+import ProgressBar from "./progress-bar.component";
+
+
 
 function App() {
+  const [completed, setCompleted] = useState(0);   // This goes with the ProgressBar
+
+  useEffect(() => {    
+    setInterval(() => setCompleted(Math.floor (Math.random() * 100) + 1), 1000);     
+  }, []);     
+  // (Math.random() * 100)
   return (
     <div>
       <div>
@@ -16,6 +27,10 @@ function App() {
       <div>
         <PieCharty/>
       </div>
+      <div className="App">
+        <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />    
+      </div>
+
     </div>
 
   )
